2bcb2ce73b3da6bf3acf67f1ddb5026879727630
cab404 <cab404@ya.ru>
2014-04-08 05:01:48 +0400

    Убрано лишнее логгирование в комментариях.
    (cherry picked from commit b911e60333de4ff3aed98d04856625d8f86eb762)
    
    Conflicts:
    	build.xml
    	src/com/cab404/libtabun/parts/Comment.java

--- src/com/cab404/libtabun/parts/Comment.java
+++ src/com/cab404/libtabun/parts/Comment.java
@@ -7,7 +7,7 @@ import com.cab404.libtabun.facility.RequestFactory;
 import com.cab404.libtabun.facility.ResponseFactory;
 import com.cab404.libtabun.util.SU;
 import com.cab404.libtabun.util.U;
-import com.cab404.libtabun.util.modular.HandledParser;
+import com.cab404.libtabun.util.html_parser.Tag;
 import org.json.simple.JSONObject;
 
 /**
@@ -30,76 +30,44 @@ public class Comment extends Part {
     /**
      * Парсит комментарии с <s>1823 года</s> тега section
      */
-    public static class CommentParser extends HandledParser {
+    public static class CommentParser extends U.TextPartParser {
         public Comment comment = new Comment();
-        int part = 0;
-        StringBuilder text = new StringBuilder();
 
-        @Override
-        public boolean line(String line) {
-            switch (part) {
-                case 0:
-                    // Находим заголовок
-                    if (line.contains("<section id=\"comment_id")) {
-                        comment.id = U.parseInt(SU.sub(line, "_id_", "\""));
-                        text.append(line).append('\n');
-                        part++;
-                    }
-                    break;
+        @Override public void process(StringBuilder out) {
+            HTMLTree parser = new HTMLTree(out.toString());
 
-                case 1:
-                    if (line.contains("</section>")) {
-                        text.append(line).append('\n');
+            comment.id = U.parseInt(parser.get(0).get("id").replace("comment_id_", ""));
 
-                        HTMLTree parser = new HTMLTree(text.toString());
-
-                        // Если комментарий пуст (совсем-совсем, не только текст) - это остов убитого модерастией.
-                        try {
-                            parser.getTagIndexByProperty("class", "text");
-                        } catch (Throwable e) {
-                            comment.MODERASTIA = true;
-                            return false;
-                        }
-
-
-                        String props = parser.getTagByName("section").props.get("class");
-                        comment.is_new = props.contains("comment-new");
-
-                        comment.body = parser.getContents(parser.getTagIndexByProperty("class", "text")).replaceAll("\t", "");
-                        // Тут чуточку сложнее.
-                        comment.time = parser.getTree(parser.getTagIndexByProperty("class", "comment-date")).getTagByName("time").props.get("datetime");
-
-                        // Достаём автора и аватарку.
-
-                        HTMLTree author;
-                        author = parser.getTree(parser.getTagIndexByProperty("class", "comment-info"));
-                        {
-                            comment.author = SU.bsub(author.getTagByName("a").props.get("href"), "profile/", "/");
-                            comment.avatar = author.getTagByProperty("alt", "avatar").props.get("src");
-                        }
-
-                        // Попытка достать род. комментарий:
-                        try {
-                            HTMLTree comment_parent_goto = parser.getTree(parser.getTagIndexByProperty("class", "goto goto-comment-parent"));
-                            comment.parent = U.parseInt(SU.bsub(comment_parent_goto.getTagByName("a").props.get("onclick"), ",", ");"));
-                        } catch (Throwable e) {
-                            comment.parent = 0;
-                        }
-
-//                        comment.votes = U.parseInt(parser.getContents(parser.getTagByProperty("class", "vote-count")).trim());
-
-
-                        return false;
-
-                    } else this.text.append(line).append("\n");
-
-                    break;
+            try {
+                comment.body = parser.getContents(parser.xPathFirstTag("section/div/div&class=*text*"));
+            } catch (Exception ex) {
+                comment.MODERASTIA = true;
             }
 
-            return true;
+            HTMLTree info =
+                    parser.getTree(parser.xPathFirstTag("ul&class=comment-info"));
+
+            Tag parent = info.xPathFirstTag("li&class=*parent*/a");
+            if (parent == null)
+                comment.parent = 0;
+            else
+                comment.parent = U.parseInt(SU.bsub(parent.get("onclick"), ",", ");"));
+
+            comment.author = SU.bsub(info.xPathFirstTag("li/a").get("href"), "profile/", "/");
+            comment.is_new = parser.get(0).get("class").contains("comment-new");
+            comment.time = info.xPathFirstTag("li/time").get("datetime");
+            comment.avatar = info.xPathFirstTag("li/a/img").get("src");
+            comment.votes = U.parseInt(info.getContents(info.xPathFirstTag("li/span&class=vote-count")));
+
         }
         @Override public void finished() {
-
+        }
+        
+        @Override public boolean isStart(String line) {
+            return line.contains("<section id=\"comment_id");
+        }
+        @Override public boolean isEnd(String line) {
+            return line.contains("</section>");
         }
     }
 
@@ -115,7 +83,9 @@ public class Comment extends Part {
                                 .addReferer(post.key.address)
                                 .setBody(body)
                                 .XMLRequest()
-                                .build()));
+                                .build()
+                )
+        );
 
         JSONObject object = MessageFactory.processJSONwithMessage(request);
 
@@ -140,7 +110,9 @@ public class Comment extends Part {
                                 .addReferer(key.address)
                                 .setBody(body)
                                 .XMLRequest()
-                                .build()));
+                                .build()
+                )
+        );
 
         JSONObject object = MessageFactory.processJSONwithMessage(request);
 
