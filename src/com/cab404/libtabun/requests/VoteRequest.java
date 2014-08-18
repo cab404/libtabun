b7403f3de324ee28051f12b6fde58633387da499
cab404 <me@cab404.ru>
2014-08-19 03:13:11 +0400

    Добавлен автопарсинг пришедших с респонсами сообщений.

--- src/com/cab404/libtabun/requests/VoteRequest.java
+++ src/com/cab404/libtabun/requests/VoteRequest.java
@@ -3,6 +3,7 @@ package com.cab404.libtabun.requests;
 import com.cab404.libtabun.data.Type;
 import com.cab404.moonlight.framework.AccessProfile;
 import com.cab404.moonlight.framework.EntrySet;
+import org.json.simple.JSONObject;
 
 /**
  * @author cab404
@@ -10,26 +11,32 @@ import com.cab404.moonlight.framework.EntrySet;
 public class VoteRequest extends LSRequest {
 
 
-    private final int id;
-    private final int vote;
-    private final Type type;
+	private final int id;
+	private final int vote;
+	private final Type type;
+	public int result = -9000;
 
-    /**
-     * @param type Константа из {@link com.cab404.libtabun.data.Type}
-     */
-    public VoteRequest(int id, int vote, Type type) {
-        this.id = id;
-        this.vote = vote;
-        this.type = type;
-    }
+	/**
+	 * @param type Константа из {@link com.cab404.libtabun.data.Type}
+	 */
+	public VoteRequest(int id, int vote, Type type) {
+		this.id = id;
+		this.vote = vote;
+		this.type = type;
+	}
 
-    @Override protected void getData(EntrySet<String, String> data) {
-        data.put("value", vote + "");
-        data.put("id" + type.name, id + "");
-    }
+	@Override protected void handle(JSONObject object) {
+		super.handle(object);
+		result = Integer.parseInt(object.get("iRating") + ""); // Ибо Long, ибо нафиг.
+	}
 
-    @Override protected String getURL(AccessProfile profile) {
-        return "/ajax/vote/" + type.name.toLowerCase() + "/";
-    }
+	@Override protected void getData(EntrySet<String, String> data) {
+		data.put("value", vote + "");
+		data.put("id" + type.name, id + "");
+	}
+
+	@Override protected String getURL(AccessProfile profile) {
+		return "/ajax/vote/" + type.name.toLowerCase() + "/";
+	}
 
 }
