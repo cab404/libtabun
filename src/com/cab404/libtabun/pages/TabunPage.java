e33b9826752627cd51c7bf14ca6cd2677d9e2138
cab404 <me@cab404.ru>
2014-09-20 09:46:48 +0400

    Адаптивный lskey yay!!!111asd

--- src/com/cab404/libtabun/pages/TabunPage.java
+++ src/com/cab404/libtabun/pages/TabunPage.java
@@ -1,9 +1,12 @@
 package com.cab404.libtabun.pages;
 
 import com.cab404.libtabun.data.CommonInfo;
+import com.cab404.libtabun.data.LSKeyModule;
 import com.cab404.libtabun.modules.CommonInfoModule;
 import com.cab404.libtabun.modules.ErrorModule;
 import com.cab404.libtabun.modules.PaginatorModule;
+import com.cab404.libtabun.requests.LSRequest;
+import com.cab404.moonlight.framework.AccessProfile;
 import com.cab404.moonlight.framework.ModularBlockParser;
 import com.cab404.moonlight.framework.Page;
 import org.apache.http.HttpResponse;
@@ -24,7 +27,7 @@ public class TabunPage extends Page {
 	public static final int
 			BLOCK_QUOTE = 279,
 			BLOCK_COMMON_INFO = 280,
-//			BLOCK_LS_KEY = 281,
+			BLOCK_LS_KEY = 281,
 			BLOCK_COMMENT = 282,
 			BLOCK_TOPIC_HEADER = 283,
 			BLOCK_USER_INFO = 284,
@@ -45,14 +48,24 @@ public class TabunPage extends Page {
 		return "/error";
 	}
 
+	private AccessProfile profile;
+	@Override public void fetch(AccessProfile accessProfile) {
+		profile = accessProfile;
+		super.fetch(accessProfile);
+	}
+
 	@Override protected void bindParsers(ModularBlockParser base) {
 		base.bind(new CommonInfoModule(), BLOCK_COMMON_INFO);
-		base.bind(new ErrorModule(), BLOCK_ERROR);
 		base.bind(new PaginatorModule(), BLOCK_PAGINATION);
+		base.bind(new LSKeyModule(), BLOCK_LS_KEY);
+		base.bind(new ErrorModule(), BLOCK_ERROR);
 	}
 
 	@Override public void handle(Object object, int key) {
 		switch (key) {
+			case BLOCK_LS_KEY:
+				profile.cookies.put(LSRequest.LS_KEY_ENTRY, String.valueOf(object));
+				break;
 			case BLOCK_COMMON_INFO:
 				this.c_inf = (CommonInfo) object;
 				break;
